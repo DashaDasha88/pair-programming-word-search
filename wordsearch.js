88ddcff2d6e806dfa1abdb9e9b29434ce18321fd
DashaDasha88 <dasha.kotova.mail@gmail.com>
2021-02-10 22:44:26 +0000

    word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return false;
+    }
+    const colLength = letters.length;
+    const rowLength = letters[0].length;
+    let found = false;
+    for (let col = 0; col < rowLength; col++){
+        let tempArray = [];
+
+        for (let row = 0; row < colLength; row++){
+            tempArray.push(letters[row][col]);
+            
+        }
+        const verticalJoin = tempArray.join('')
+        //console.log(tempArray.join(''));
+        //console.log(verticalJoin);
+        if (verticalJoin.includes(word)) {
+            found = true;
+        }
+        //console.log(tempArray);
+    }
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+        found = true;
+        }
     }
+    return found;
 }
+
+const arr = [];
+const word = 'FRANK';
+
+console.log(wordSearch(arr, word));
 
 module.exports = wordSearch
